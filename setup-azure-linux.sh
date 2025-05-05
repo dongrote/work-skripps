8f3d0e7dd674237b2a5695185b9e77f0d18748aa
Don Grote <don.grote@gmail.com>
2025-05-05 14:49:49 -0400

    Update setup-azure-linux.sh

--- setup-azure-linux.sh
+++ setup-azure-linux.sh
@@ -184,7 +184,7 @@ vim.opt.rtp:prepend(lazypath)
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = "\\\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -198,6 +198,78 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/plugins/nvim-tree.lua
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("nvim-tree").setup {
+      view = {
+        float = { enable = true, },
+        width = {
+          min = 30,
+          max = -1,
+        },
+      },
+      actions = {
+        open_file = { quit_on_open = true, },
+      },
+    }
+  end,
+}
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/plugins/treesitter.lua
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                "jsdoc", "bash", "c_sharp"
+            },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            indent = {
+                enable = true
+            },
+
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = { "markdown" },
+            },
+        })
+
+        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        treesitter_parser_config.templ = {
+            install_info = {
+                url = "https://github.com/vrischmann/tree-sitter-templ.git",
+                files = {"src/parser.c", "src/scanner.c"},
+                branch = "master",
+            },
+        }
+
+        vim.treesitter.language.register("templ", "templ")
+    end
+}
 EOF
 
   cat <<EOF >$HOME/.config/nvim/lua/dongrote/plugins/telescope.lua
