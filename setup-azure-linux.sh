dc91e0fb48b7be2e8f44eac5873701da955c47a1
Don Grote <don.grote@gmail.com>
2025-05-05 14:40:35 -0400

    Update setup-azure-linux.sh

--- setup-azure-linux.sh
+++ setup-azure-linux.sh
@@ -9,6 +9,247 @@ install_neovim() {
     ln -s $HOME/nvim-linux-x86_64/bin/nvim $HOME/bin/nvim
 }
 
+setup_neovim() {
+  if [ -d $HOME/.config/nvim ] ; then
+    rm -rf $HOME/.config/nvim
+  fi
+  mkdir -p $HOME/.config/nvim/lua/dongrote/plugins
+  cat <<EOF >$HOME/.config/nvim/init.lua
+require('dongrote.autocmds')
+require('dongrote.remap')
+require('dongrote.set')
+require('dongrote.lazy')
+EOF
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/autocmds.lua
+-- Define an autocommand group to organize your autocmds
+local augroup = vim.api.nvim_create_augroup("BuildKeymapGroup", { clear = true })
+
+-- Rust-specific keymap
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  group = augroup,
+  callback = function()
+    vim.keymap.set("n", "<leader>b", function()
+      vim.cmd("!cargo b")
+    end, { desc = "Run cargo build", buffer = true })
+    vim.keymap.set("n", "<leader>B", function()
+      vim.cmd("!cargo b -r")
+    end, { desc = "Run cargo release build", buffer = true })
+    vim.keymap.set("n", "<leader>t", function()
+      vim.cmd("!cargo t")
+    end, { desc = "Run cargo test", buffer = true })
+  end,
+})
+
+-- C#-specific keymap
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  group = augroup,
+  callback = function()
+    vim.keymap.set("n", "<leader>b", function()
+      vim.cmd("!dotnet build")
+    end, { desc = "Run dotnet build", buffer = true })
+    vim.keymap.set("n", "<leader>B", function()
+      vim.cmd("!dotnet build -c Release")
+    end, { desc = "Run dotnet release build", buffer = true })
+    vim.keymap.set("n", "<leader>t", function()
+      vim.cmd("!dotnet test")
+    end, { desc = "Run dotnet test", buffer = true })
+  end,
+})
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/remap.lua
+vim.g.mapleader = " "
+
+-- split window
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split window horizontally" })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
+-- switch windows
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
+-- show full path
+vim.keymap.set("n", "<leader>P", "1<C-g>", { desc = "show full file path" })
+
+-- open file explorer
+vim.keymap.set("n", "<leader>us", "<cmd>:setlocal spell<CR>", { desc = "Spellcheck On", })
+vim.keymap.set("n", "<leader>uS", "<cmd>:setlocal nospell<CR>", { desc = "Spellcheck Off", })
+
+-- open file explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle, { desc = "File Tree Toggle", })
+
+-- set executable bit on current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- run custom test.sh script
+-- vim.keymap.set("n", "<leader>t", "<cmd>!test.sh %<CR>", { desc = "Run Tests", })
+-- vim.keymap.set("n", "<leader>b", "<cmd>!build.sh %<CR>", { desc = "Run Build", })
+
+-- clear highlight
+vim.keymap.set("n", "<ESC>", "<cmd>noh<CR>", { desc = "Clear Highlight", })
+
+-- csharp (dotnet) shell commands
+vim.keymap.set("n", "<leader>cst", "<cmd>!dotnet test<CR>", { desc = "Run C# Tests", })
+vim.keymap.set("n", "<leader>csb", "<cmd>!dotnet build<CR>", { desc = "C# Build", })
+vim.keymap.set("n", "<leader>csr", "<cmd>!dotnet restore<CR>", { desc = "C# Restore", })
+
+-- nodejs shell commands
+vim.keymap.set("n", "<leader>npmb", "<cmd>!npm run build<CR>")
+vim.keymap.set("n", "<leader>npmt", "<cmd>!npm test<CR>")
+
+-- lsp stuffs
+vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format", })
+
+-- vim.keymap.set("n", "gD", vim.lsp.buf.declaration,  "Go to declaration")
+-- vim.keymap.set("n", "gd", vim.lsp.buf.definition,  "Go to definition")
+-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation,  "Go to implementation")
+-- vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help,  "Show signature help")
+-- vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,  "Add workspace folder")
+-- vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,  "Remove workspace folder")
+
+-- vim.keymap.set("n", "<leader>gd", "<cmd>lua require('omnisharp_extended').telescope_lsp_definition({ jump_type = 'vsplit' })<CR>")
+-- navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Comment
+vim.keymap.set("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+vim.keymap.set("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
+-- telescope
+vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+vim.keymap.set("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "telescope find files" })
+vim.keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+vim.keymap.set("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+vim.keymap.set("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+vim.keymap.set("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/set.lua
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = '80'
+
+-- Trying to change line numbers to be more visible
+-- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white', })
+-- vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white', })
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+
+-- automatically create folds based on indentation
+vim.opt.foldmethod = 'indent'
+vim.opt.foldcolumn = '4'
+vim.opt.foldlevelstart = 2
+
+-- disable virtual text because lsp_lines makes them redundant
+vim.diagnostic.config({ virtual_text = false })
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/lazy.lua
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "dongrote/plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/plugins/telescope.lua
+return {
+    "nvim-telescope/telescope.nvim",
+
+    tag = "0.1.5",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find Files", })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
+}
+EOF
+
+  cat <<EOF >$HOME/.config/nvim/lua/dongrote/plugins/which-key.lua
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({global = false})
+			end,
+			desc = "Buffer local Keymaps (which-key)",
+		},
+	},
+}
+EOF
+}
+
 setup_tmux() {
   sudo dnf install -y tmux
 
